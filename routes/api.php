ffa09d70e4c78572725a3dc114c6a68cfee132fb
pornsiri2536 <pornsiri.vei@vru.ac.th>
2025-10-05 11:29:40 +0700

    api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return ['message' => 'API route is working'];
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('api')->get('/user', function (Request $request) {
+    return $request->user();
 });
